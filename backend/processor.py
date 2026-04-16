8a293c6aa1a3c7bf6874dccb29a428e9c9c6f344
Hamood-pixel <tcs166214@gmail.com>
2026-04-16 22:51:56 +0500

    PDF read and converted to string

---
+++ backend/processor.py
@@ -0,0 +1,7 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+def process_pdf(file_path: str):
+    loader = PyPDFLoader(file_path)
+    pages = loader.load()
+    return pages 
