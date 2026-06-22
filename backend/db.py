0a2655fe7b68b4da3f1644e06d9fadbe2c6c7078
Hamood-pixel <tcs166214@gmail.com>
2026-06-22 17:07:52 +0500

    Database done, search and creating embeddings function made

--- backend/db.py
+++ backend/db.py
@@ -10,7 +10,17 @@ def create_embeddings(chunks, db_path = db_directory):
     """
     Takes in chunks, create vector embeddings and stores them in the vector database whose path is defined in the arguments
     """  
-    return 
+    vector_db = Chroma.from_documents(docements = chunks, embedding = embeddings, persist_directory = db_path) #use chroma class to create vector embeddings
+
+    return vector_db
+
+def get_retriever(db_path = db_directory):
+    """
+    Takes in the path to the vector database and returns a retriever object that can be used to retrieve relevant chunks based on a query
+    """
+    vector_db = Chroma(persist_directory = db_path, embedding_function = embeddings)
+
+    return vector_db.as_retriever(search_kwargs = {"k": 4}) #returns 4 relevant chunks based on the query 
 
 
 
