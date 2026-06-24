5dbaf3bb466bf358c8b26a1281547a026196634c
Hamood-pixel <tcs166214@gmail.com>
2026-06-24 10:03:29 +0500

    LLM attached, llama3

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI, UploadFile, File #acts as chef working with backend
 import uvicorn #handles the server routing, acts as waiter
 
 from langchain_ollama import OllamaLLM
-
+from db import get_retriever  #retriever is the function we created in db.py to get the retriever object which would be used to fetch relevant chunks
 
 #importing the functions from the files we made in backend
 from processor import process_pdf
@@ -37,3 +37,34 @@ async def upload_file(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     uvicorn.run(app, host = "0.0.0.0", port = 8000) #run the app on localhost:8000
+
+@app.post("/query") #API endpoint to handle user queries, this would be called by the frontend to talk to the backend and get answers from the LLM
+async def query_paper(payload: dict): #payload is the actual data to be sent to the LLM
+    """
+    Takes user query, converts it to embeddings, retrieves relevant chunks and generate response using LLM. 
+    """
+    user_question = payload.get("question") 
+
+    if not user_question: 
+        return {"status": "error", "message": "No question provided."}
+    
+    try: 
+        retriver = get_retriever() #get the retriever object from the db.py file
+        relevant_chunks = get_retriever.invoke(user_question)
+        
+        #combine the relevant chunks into a single string to send to the LLM
+        context = " ".join([chunk.page_content for chunk in relevant_chunks])
+
+        #create a prompt for the LLM 
+        prompt = f"You are a Research Assistant. Answer the User's question using ONLY the provided context from the research paper. If the answer is not in the context or you dont know the answer, say 'I don't know'. Donot make things up. \n\nContext: {context}\n\nUser's Question: {user_question}\n\nAnswer:"
+
+        #initiliaze LLM model 
+        llm = OllamaLLM(model = "llama3")
+
+        #run model on GPU
+        response = llm.invoke(prompt)
+
+        return {"status": "success", "answer": response}
+    except Exception as e:
+        {"status": "error", "message": str(e)}
+        
